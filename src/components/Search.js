ad42ee6496350cbf50fd5c8f14da25ef57c2f2e1
jay32den <siskjayden09@gmail.com>
2022-12-21 21:01:48 -0600

    initial commit

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,12 @@
+import React from 'react';
 import SearchIcon from '@material-ui/icons/Search';
 import MicIcon from '@material-ui/icons/Mic';
 import { Button } from '@material-ui/core';
 import styled from 'styled-components';
-import React from 'react';
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useStateValue } from '../SearchProvider';
+import { useNavigate} from 'react-router-dom';
+import { useStateValue } from '../SearchProvider.js';
 import { SET_SEARCH_TERM } from '../types.js';
+import { useState } from 'react';
 
 const SearchInput = styled.div`
     display: flex;
@@ -19,21 +19,21 @@ const SearchInput = styled.div`
     border-radius: 999px;
     input {
         flex: 1;
-        padding: 8px 13px;
+        paddinh: 8px 13px;
         font-size: medium;
         border: 0;
         outline: 0;
     }
     .searchicon {
-        color: gray;
+        color:gray;
     }
-`;
+`
+
 
 const SearchButton = styled.div`
     margin-top: 20px;
-    dislplay: flex;
+    display: flex;
     justify-content: center;
-    margin-left:932px;
     button {
         margin: 5px;
         background: #f8f8f8 !important;
@@ -43,43 +43,43 @@ const SearchButton = styled.div`
             margin: 5px;
             background: #f8f8f8 !important;
             color: #000;
-            border: 1px solid #c6c6c6;
+            border: 1px solid #c6c6c6
         }
     }
-`;
+`
+
 
 const Search = ({ hide }) => {
     const [input, setInput] = useState("");
     const navigate = useNavigate();
-    const [{}, dispatch] = useStateValue();
+    const[{}, dispatch] = useStateValue();
 
     const search = e => {
         e.preventDefault();
         dispatch({
-            type:SET_SEARCH_TERM,
+            type: SET_SEARCH_TERM,
             term: input
         })
         console.log(input);
-        navigate("/search");
+         navigate("/search");
     }
 
     return (
         <form onSubmit={search}>
             <SearchInput>
-            <SearchIcon className = 'searchicon' />
-            <input value = {input} onChange = {e => setInput(e.target.value)} />
-            <MicIcon />
-            </SearchInput>
-            {
-                !hide && (
+                <SearchIcon className='searchicon' />
+                <input value={input} onChange={e => setInput(e.target.value)} />
+                <MicIcon />
+                </SearchInput>
+                {!hide && (
                     <SearchButton>
-                        <Button type = "submit" variant = "outlined">Google Search</Button>
-                        <Button variant = "outlined">I am feeling lucky</Button>
+                        <Button type="submit" variant="outlined">Google Search</Button>
+                        <Button variant="outlined">I am feeling lucky</Button>
                     </SearchButton>
                 )
             }
         </form>
     )
-};
+}
 
 export default Search;
