f9d5aa3f882a7d9803f3f1e67a55b327fc677d46
jay32den <siskjayden09@gmail.com>
2023-08-02 13:44:30 -0500

    new commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { SearchProvider } from './SearchProvider';
 import reducer, { initialState } from './reducer';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+ReactDOM.render(
   <SearchProvider initialState={initialState} reducer={reducer}>
     <App />
-  </SearchProvider>
+  </SearchProvider>,
+  document.getElementById('root')
 );
